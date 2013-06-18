51e7c971dd12188ac92d85fd6b767e71912feb2e
이항희 <javarouka@nuritelecom.com>
2013-06-18 15:23:29 +0900

    기본 구성 추가

--- roux.js
+++ roux.js
@@ -1,57 +1,8 @@
 /**
- * Created with JetBrains WebStorm.
- * User: javarouka
- * Date: 13. 6. 3
- * Time: 오전 2:28
- * To change this template use File | Settings | File Templates.
  */
-
-
-(function(){
+(function(context) {
 
   "use strict";
-
-  var roux = {};
-
-  var types = [ 'Number', 'String', 'Boolean', 'Array', 'Date', 'RegExp' ],
-      ownKey = Object.prototype.hasOwnProperty,
-      toString = Object.prototype.toString;
-  for(var t = 0,len = types.length; t < len; t++) {
-    (function(type) {
-      roux['is' + type] = function(obj) {
-        return toString.call(obj) === '[object ' + type + ']';
-      }
-    })(types[t]);
-  }
-  roux.isArray = Array.isArray || function(obj) {
-    return toString.call(obj) === '[object Array]'
-  };
-  roux.isObject = function(obj) {
-    return obj === Object(obj);
-  };
-
-  // Object =========================================================
-
-  roux.each = function(obj, iter) {
-    if(obj.forEach) {
-      obj.forEach(iter);
-    }
-    else if(roux.isArray(obj)) {
-      for(var i = 0, len = obj.length; i < len; i = i + 1) {
-        iter.call(obj, obj[i], i);
-      }
-    }
-    else if(roux.isObject(obj)) {
-      var k;
-      for(k in obj) {
-        if(!ownKey.call(obj, k)) continue;
-        iter.call(obj, obk[k], k);
-      }
-    }
-    else {
-      iter(obj);
-    }
-  };
 
   Object.keys = Object.keys || function(obj) {
     var ret = [];
@@ -62,23 +13,101 @@
     return ret;
   };
 
+  Object.create = Object.create || function(obj) {
+    var object = function(){};
+    object.prototype = obj;
+    return new object();
+  };
+
+  var root = context;
+  var raop = Object.create(null);
+  var preventConflictName = root.raop;
+
+  if (typeof exports !== 'undefined') {
+    if (typeof module !== 'undefined' && module.exports) {
+      exports = module.exports = raop;
+    }
+    exports.raop = raop;
+  }
+  else {
+    root.raop = raop;
+  }
+
+  var types = [ 'Number', 'String', 'Boolean', 'Array', 'Date', 'RegExp' ],
+      ownKey = Object.prototype.hasOwnProperty,
+      toString = Object.prototype.toString;
+  for(var t = 0,len = types.length; t < len; t++) {
+    (function(type) {
+      raop['is' + type] = function(obj) {
+        return toString.call(obj) === '[object ' + type + ']';
+      }
+    })(types[t]);
+  }
+  raop.isArray = Array.isArray || function(obj) {
+    return toString.call(obj) === '[object Array]'
+  };
+  raop.isObject = function(obj) {
+    return obj === Object(obj);
+  };
+
+  raop.noConflict = function() {
+    root.raop = preventConflictName;
+    return this;
+  };
+
+  // Object =========================================================
+
+  raop.each = function(obj, iter) {
+    if(obj.forEach) {
+      obj.forEach(iter);
+    }
+    else if(raop.isArray(obj)) {
+      for(var i = 0, len = obj.length; i < len; i = i + 1) {
+        iter.call(obj, obj[i], i);
+      }
+    }
+    else if(raop.isObject(obj)) {
+      var k;
+      for(k in obj) {
+        if(!ownKey.call(obj, k)) continue;
+        iter.call(obj, obj[k], k);
+      }
+    }
+    else {
+      iter(obj);
+    }
+  };
+
+  raop.extend = function(dst, src) {
+    raop.each(src, function(v, k) {
+      dst[k] = v;
+    });
+    return obj;
+  };
+
   // Object end =========================================================
 
   // AOP =========================================================
 
-  var Pointcut = function() {
+  var Pointcut = function(exprssion) {
   };
 
-  var Advise = function() {
+  var Advise = function(action) {
   };
 
-  var Aspect = function() {
+  var Aspect = function(pointcut, action) {
+    this.pointcut = pointcut;
+    this.action = action;
   };
 
-  var aop = function(namespace) {
-
+  Aspect.prototype = {
+    after: function() {},
+    before: function() {},
+    around: function() {},
+    error: function() {}
   };
+  Aspect.prototype.constructor = Aspect;
 
   // AOP end =========================================================
 
-})();
+})(this);
