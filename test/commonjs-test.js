f7c6be9a6b07bd132a39856dc923120e1be8ef4d
javarouka(이항희) <javarouka@coupang.com>
2014-01-22 15:17:18 +0900

    AMD 버그픽스

--- test/commonjs-test.js
+++ test/commonjs-test.js
@@ -41,3 +41,193 @@ exports.addTargetTest = function(test) {
   test.done();
 
 };
+
+exports.addValidator = function(test) {
+
+  var calculator = {
+    plus: function(a, b) {
+      return a + b;
+    },
+    minus: function(a, b) {
+      return a - b;
+    }
+  };
+
+  var argumentCheck = function(options) {
+    var args = options.args;
+    for(var i = 0, len = args.length; i < len; i++) {
+      if(typeof args[i] !== 'number') {
+        throw new TypeError("all arguments must be number type");
+      }
+    }
+  };
+
+  crosscutting.before(
+    calculator,
+    function() {
+      return true;
+    },
+    argumentCheck
+  );
+
+  try {
+    calculator.plus("one", "two");
+    test.ok(false, "validator test failed...method success...");
+  }
+  catch(ok) {
+    test.ok(ok instanceof TypeError, "validator test success!!!!");
+  }
+
+  test.done();
+
+};
+
+exports.testAroundType = function(test) {
+
+  var obj = {
+    a: function(one) {
+      return one;
+    },
+    b: function(one) {
+      return one;
+    }
+  };
+
+  crosscutting.around(
+    obj,
+    new crosscutting.Aspect.Pointcut(/a/),
+    function(todo/*, options*/) {
+      return todo();
+    }
+  );
+
+  crosscutting.around(
+    obj,
+    new crosscutting.Aspect.Pointcut(/b/),
+    function(todo/*, options*/) {
+      return todo(100) + 1;
+    }
+  );
+
+  test.ok(obj.a(1) === 1, "method a aop apply");
+  test.ok(obj.b(100) === 101, "method b aop apply");
+  test.done();
+};
+
+exports.testApplyAOPBasic = function(test) {
+
+  var obj = {
+    a: function(one) {
+      return one;
+    },
+    b: function(two) {
+      return two;
+    },
+    c: function(three) {
+      return three;
+    }
+  };
+
+  crosscutting.before(
+    obj,
+    /^(a+|c+)/,
+    function(options) {
+      options.args[0] = 1000;
+    }
+  );
+
+  test.ok(obj.a(1) === 1000, "method a aop apply");
+  test.ok(obj.b(2) === 2, "method b aop no-apply");
+  test.ok(obj.c(3) === 1000, "method a aop apply");
+
+  test.done();
+};
+
+exports.logTest = function(test) {
+
+  var calculator = {
+    plus: function(a, b) {
+      return a + b;
+    },
+    minus: function(a, b) {
+      return a - b;
+    }
+  };
+
+  var isLogging = false;
+
+  var logAdvice = function(options) {
+    isLogging = true;
+  };
+  crosscutting.before(
+    calculator,
+    function() {
+      return true;
+    },
+    logAdvice
+  );
+  calculator.plus(1, 1);
+
+  test.ok(isLogging, "ok Logging!");
+  test.done();
+};
+
+exports.testException = function(test) {
+  var calculator = {
+    plus: function(a, b) {
+      if(typeof a !== 'number' || typeof b !== 'number') {
+        throw new Error("arguments must be number type");
+      }
+      return a + b;
+    },
+    minus: function(a, b) {
+      if(typeof a !== 'number' || typeof b !== 'number') {
+        throw new Error("arguments must be number type");
+      }
+      return a - b;
+    }
+  };
+
+  var exception = null;
+  var throwHandler = function(options) {
+    exception = options.exception;
+//    console.log("Error! " + exception.name + ". cause : " + exception.message);
+//    if(exception.stack) console.log(exception.stack);
+  };
+  // all method aop apply, type BEFORE
+  crosscutting.exception(
+    calculator,
+    /^plus/,
+    throwHandler
+  );
+  var result = calculator.plus("one", "two");
+
+  test.ok(exception, "error was thrown by method");
+
+  test.done();
+};
+
+exports.testNewState = function(test) {
+
+  var obj = {
+    a: function(one) {
+      return one;
+    }
+  };
+
+  crosscutting.before(
+    obj,
+    function(value/*, target*/) {
+      return !!value;
+    },
+    function(options) {
+      options.args[0] = 1000;
+    }
+  );
+
+  test.ok(obj.a(1) === 1000, "method a aop apply");
+
+  test.done();
+
+};
+
